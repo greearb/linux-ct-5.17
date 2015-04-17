42e68910160e68bba5f3bd47ff9fb08afd3d13f3
Ben Greear <greearb@candelatech.com>
2015-04-17 15:35:05 -0700

    ath9k: Allow ADHOC + AP combinations.
    Patch borrowed from OpenWrt project.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath9k/init.c
+++ drivers/net/wireless/ath/ath9k/init.c
@@ -887,6 +887,7 @@ static const struct ieee80211_iface_limit if_limits[] = {
 				 BIT(NL80211_IFTYPE_AP) },
 	{ .max = 1,	.types = BIT(NL80211_IFTYPE_P2P_CLIENT) |
 				 BIT(NL80211_IFTYPE_P2P_GO) },
+	{ .max = 1,     .types = BIT(NL80211_IFTYPE_ADHOC) },
 };
 
 #ifdef CONFIG_ATH9K_CHANNEL_CONTEXT
