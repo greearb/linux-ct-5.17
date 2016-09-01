d7d38af60d926093be74095ac9ef67bbc424083c
Ben Greear <greearb@candelatech.com>
2016-09-01 16:25:11 -0700

    ath10k: Improve nohwcrypt logic for CT firmware.
    This properly lets fwcfg control enabling/disabling the hwcrypt
    option.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- drivers/net/wireless/ath/ath10k/wmi.c
+++ drivers/net/wireless/ath/ath10k/wmi.c
@@ -6753,7 +6753,7 @@ static struct sk_buff *ath10k_wmi_10_1_op_gen_init(struct ath10k *ar)
 		     ar->running_fw->fw_file.fw_features)) {
 		if (test_bit(ATH10K_FW_FEATURE_CT_RXSWCRYPT,
 			     ar->running_fw->fw_file.fw_features) &&
-		    ath10k_modparam_nohwcrypt) {
+		    ar->request_nohwcrypt) {
 			/* This will disable rx decryption in hardware, enable raw
 			 * rx mode, and native-wifi tx mode.  Requires 'CT' firmware.
 			 */
@@ -6863,7 +6863,7 @@ static struct sk_buff *ath10k_wmi_10_2_op_gen_init(struct ath10k *ar)
 		     ar->running_fw->fw_file.fw_features)) {
 		if (test_bit(ATH10K_FW_FEATURE_CT_RXSWCRYPT,
 			     ar->running_fw->fw_file.fw_features) &&
-		    ath10k_modparam_nohwcrypt) {
+		    ar->request_nohwcrypt) {
 			/* This will disable rx decryption in hardware, enable raw
 			 * rx mode, and native-wifi tx mode.  Requires 'CT' firmware.
 			 */
@@ -6989,7 +6989,7 @@ static struct sk_buff *ath10k_wmi_10_4_op_gen_init(struct ath10k *ar)
 #endif
 		if (test_bit(ATH10K_FW_FEATURE_CT_RXSWCRYPT,
 			     ar->running_fw->fw_file.fw_features) &&
-		    ath10k_modparam_nohwcrypt) {
+		    ar->request_nohwcrypt) {
 			/* This will disable rx decryption in hardware, enable raw
 			 * rx mode, and native-wifi tx mode.  Requires 'CT' firmware.
 			 */
